b6f40a566c67de5568f869787a6495321ce2920a
Anton <anton@Air-Anton.mshome.net>
2018-10-28 14:03:17 +0200

    f

---
+++ precache-manifest.68bb56344545c836b3cc18f2277c2a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "76cab9ad0694632167ac",
+    "url": "/static/js/main.76cab9ad.chunk.js"
+  },
+  {
+    "revision": "f2aec47a2fa0f9ed3549",
+    "url": "/static/js/1.f2aec47a.chunk.js"
+  },
+  {
+    "revision": "76cab9ad0694632167ac",
+    "url": "/static/css/main.619b08a2.chunk.css"
+  },
+  {
+    "revision": "f2aec47a2fa0f9ed3549",
+    "url": "/static/css/1.a5947c9b.chunk.css"
+  },
+  {
+    "revision": "1d6c6119113a11ff611179475d3aa64a",
+    "url": "/index.html"
+  }
+];
